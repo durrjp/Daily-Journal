6c32160c19548f7cf4f05206c8d392ff3bed79cc
durrjp <durrjp1@gmail.com>
2020-03-02 14:48:59 -0600

    added id property to journal data provider

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -9,18 +9,21 @@
 // This is the original data. Can't Touch This.
 const journal = [
     {
+        id: 1,
         date: "07/24/2025",
         concept: "HTML & CSS",
         entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
         mood: "Ok"
     },
     {
+        id: 2,
         date: "02/27/2020",
         concept: "Javascript",
         entry: "We learned some new javascript including HTML rendering automation.",
         mood: "Happy"
     },
     {
+        id: 3,
         date: "02/28/2020",
         concept: "HTML & CSS",
         entry: "Steve reviewed the code Globetrotters created for our first project. The biggest feedback was the nav bar needed better CSS style naming. I created it so I'll need to fix those styles!",
