4f0ac8edb927ba057eb8dc2abc4adbd350ea96f9
durrjp <durrjp1@gmail.com>
2020-03-14 12:55:09 -0500

    completed timeline in css

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -3,14 +3,31 @@
  *           HTML representation of the data
  */
 const JournalEntryComponent = (entryitem) => {
-    return `
-        <section id="entry--${entryitem.id}" class="journalEntry">
-            <div class="entryHeader">${entryitem.date}</div>
-            <div><span class="entryDetails">Concept</span>: ${entryitem.concept}</div>
-            <div><span class="entryDetails">Entry</span>: ${entryitem.entry}</div>
-            <div><span class="entryDetails">Overal Mood</span>: ${entryitem.mood}</div>
-        </section>
-    `
+    if (entryitem.id%2 === 0) {
+        const side = "left"
+        return `
+            <div class="containers ${side}">
+                <section id="entry--${entryitem.id}" class="journalEntry content">
+                    <div class="entryHeader">${new Date(entryitem.date).toLocaleDateString('en-US')}</div>
+                    <div><span class="entryDetails">Concept</span>: ${entryitem.concept}</div>
+                    <div><span class="entryDetails">Entry</span>: ${entryitem.entry}</div>
+                    <div><span class="entryDetails">Overal Mood</span>: ${entryitem.mood}</div>
+                </section>
+            </div>
+        `
+    } else {
+        const side = "right"
+        return `
+        <div class="containers ${side}">
+            <section id="entry--${entryitem.id}" class="journalEntry content">
+                <div class="entryHeader">${new Date(entryitem.date).toLocaleDateString('en-US')}</div>
+                <div><span class="entryDetails">Concept</span>: ${entryitem.concept}</div>
+                <div><span class="entryDetails">Entry</span>: ${entryitem.entry}</div>
+                <div><span class="entryDetails">Overal Mood</span>: ${entryitem.mood}</div>
+            </section>
+        </div>
+        `
+    }
 }
 
 export default JournalEntryComponent
--- scripts/entryForm/entryForm.js
+++ scripts/entryForm/entryForm.js
@@ -1,6 +1,7 @@
 export const EntryForm = () => {
     return `
-        <form action="" class="form">
+        <div class ="form">
+        <form action="">
             <fieldset class="formitem">
                 <label for="journalDate" class="bold">Date of Entry</label>
                 <input type="date" name="journalDate" id="journalDate" class="width100">
@@ -11,19 +12,20 @@ export const EntryForm = () => {
             </fieldset>
             <fieldset class="formitem">
                 <label for="journalEntry" class="bold">Journal Entry</label>
-                <textarea rows="8" cols="60" name="journalEntry" id="journalEntry" class="width100"></textarea>
+                <textarea rows="6" cols="50" name="journalEntry" id="journalEntry" class="width100"></textarea>
             </fieldset>
             <fieldset class="formitem">
                 <label for="journalMood" class="bold">Mood for the Day</label>
                 <select name="journalMood" id="journalMood">
                     <option value="Very sad">Very sad</option>
                     <option value="Sad">Sad</option>
-                    <option value="Soso" selected>Okay</option>
+                    <option value="Okay" selected>Okay</option>
                     <option value="Happy">Happy</option>
                     <option value="PUMPED">PUMPED</option>
                 </select>
             </fieldset>
-        <button type="submit" id="submitEntry" class="button">Record Journal Entry</button>
         </form>
+        <button type="submit" id="submitEntry" class="button">Record Journal Entry</button>
+        </div>
     `
 }
