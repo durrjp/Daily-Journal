f69ec83e0edaf4589ce8d7ea46a213b9f23a8ebf
durrjp <durrjp1@gmail.com>
2020-04-10 14:32:08 -0500

    styling

--- scripts/entries/JournalEntry.js
+++ scripts/entries/JournalEntry.js
@@ -2,17 +2,15 @@
  *  Purpose: To render a single journal entry as an
  *           HTML representation of the data
  */
-const JournalEntryComponent = (entryitem, side) => {
-            return `
-            <div class="containers ${side}">
-                <section id="entry--${entryitem.id}" class="journalEntry content">
-                    <div class="entryHeader">${new Date(`${entryitem.date} CST`).toLocaleDateString('en-US')}</div>
-                    <div><span class="entryDetails">Concept</span>: ${entryitem.concept}</div>
-                    <div><span class="entryDetails">Entry</span>: ${entryitem.entry}</div>
-                    <div><span class="entryDetails">Overal Mood</span>: ${entryitem.mood}</div>
-                    <button id="deleteEntry--${entryitem.id}">Delete</button>
-                </section>
-            </div>
+const JournalEntryComponent = (entryitem) => {
+        return `
+        <section id="entry--${entryitem.id}" class="journalEntry content">
+            <div class="entryHeader">${new Date(`${entryitem.date} CST`).toLocaleDateString('en-US')}</div>
+            <div><span class="entryDetails">Concept</span>: ${entryitem.concept}</div>
+            <div><span class="entryDetails">Entry</span>: ${entryitem.entry}</div>
+            <div><span class="entryDetails">Overal Mood</span>: ${entryitem.mood}</div>
+            <button id="deleteEntry--${entryitem.id}">Delete</button>
+        </section>
         `
 }
 
--- scripts/entryForm/entryForm.js
+++ scripts/entryForm/entryForm.js
@@ -1,31 +1,32 @@
 export const EntryForm = () => {
     return `
-        <div class ="form">
-        <form action="">
-            <fieldset class="formitem">
-                <label for="journalDate" class="bold">Date of Entry</label>
-                <input type="date" name="journalDate" id="journalDate" class="width100">
-            </fieldset>
-            <fieldset class="formitem">
-                <label for="journalConcept" class="bold">Concepts covered</label>
-                <input type="text" name="journalConcept" id="journalConcept" class="width100">
-            </fieldset>
-            <fieldset class="formitem">
-                <label for="journalEntry" class="bold">Journal Entry</label>
-                <textarea rows="6" cols="50" name="journalEntry" id="journalEntry" class="width100"></textarea>
-            </fieldset>
-            <fieldset class="formitem">
-                <label for="journalMood" class="bold">Mood for the Day</label>
-                <select name="journalMood" id="journalMood">
-                    <option value="Very sad">Very sad</option>
-                    <option value="Sad">Sad</option>
-                    <option value="Okay" selected>Okay</option>
-                    <option value="Happy">Happy</option>
-                    <option value="PUMPED">PUMPED</option>
-                </select>
-            </fieldset>
-        </form>
-        <button type="submit" id="submitEntry" class="button">Record Journal Entry</button>
-        </div>
+            <div class="buttonDiv">
+                <button type="submit" id="submitEntry" class="button">Record Journal Entry</button>
+            </div>
+            <form class="form" action="">
+                <fieldset class="formitem date">
+                    <label for="journalDate" class="bold">Date of Entry</label>
+                    <input type="date" name="journalDate" id="journalDate" class="width100">
+                </fieldset>
+                <fieldset class="formitem mood">
+                    <label for="journalMood" class="bold">Mood for the Day</label>
+                    <select name="journalMood" id="journalMood">
+                        <option value="Very sad">Very sad</option>
+                        <option value="Sad">Sad</option>
+                        <option value="Okay" selected>Okay</option>
+                        <option value="Happy">Happy</option>
+                        <option value="PUMPED">PUMPED</option>
+                    </select>
+                </fieldset>
+                <fieldset class="formitem concept">
+                    <label for="journalConcept" class="bold">Concepts covered</label>
+                    <input rows="5" type="text" name="journalConcept" id="journalConcept" class="conceptstext">
+                </fieldset>
+                <fieldset class="formitem text">
+                    <label for="journalEntry" class="bold">Journal Entry</label>
+                    <textarea rows="10" cols="40" name="journalEntry" id="journalEntry" class="width100"></textarea>
+                </fieldset>
+            </form>
+            
     `
 }
